1c89dd1845eb75fae888a0ec99d7aa58a18299ba
Scar26 <mmatty26@gmail.com>
2023-02-01 01:17:22 +0530

    complete proof verification
    Signed-off-by: Scar26 <mmatty26@gmail.com>

--- src/verifier.rs
+++ src/verifier.rs
@@ -1,15 +1,34 @@
-use pairing::Engine;
+use core::ops::{AddAssign, Mul, Neg};
 
-use crate::{VerificationError, Proof, Parameters};
+use pairing::{ Engine };
+use ff::Field;
+
+
+use crate::{ VerificationKey, VerificationError, Proof };
 
 pub fn verify_proof<E: Engine>(
     proof: &Proof<E>,
-    params: &Parameters<E>,
     public_inputs: &[E::Fr],
+    vk: VerificationKey<E>,
 ) -> Result<(), VerificationError> {
-    if (public_inputs.len() + 1) != params.vk.ic.len() {
+    if (public_inputs.len() + 1) != vk.ic.len() {
         return Err(VerificationError::InvalidVerifyingKey);
     }
 
-    unimplemented!()
+    let mut acc: E::G1 = vk.ic[0].into();
+
+    for (i, b) in public_inputs.iter().zip(vk.ic.iter().skip(1)) {
+        acc.add_assign(&(*b * i));
+    }
+    
+    let mut rhs = E::pairing(&proof.a, &proof.b.into());
+    rhs.add_assign(E::pairing(&acc.into(), &vk.gamma_g2.mul(E::Fr::one().neg()).into()));
+    rhs.add_assign(E::pairing(&proof.c, &vk.delta_g2.mul(E::Fr::one().neg()).into()));
+
+    if E::pairing(&vk.alpha_g1, &vk.beta_g2) == rhs {
+        Ok(())
+    } else {
+        Err(VerificationError::InvalidProof)
+    }
+
 }
